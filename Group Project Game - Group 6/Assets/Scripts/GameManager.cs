0bc9c8e2a63460fe55e9031b10842ed87db77a4a
mrchooch <tyler-martignetti@hotmail.co.uk>
2019-02-11 18:32:27 +0000

    Added mana and tile movement

---
+++ Group Project Game - Group 6/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	public int mana = 500;
+	public Text manaText;
+
+	void Start() {
+		manaText.text = mana.ToString();
+	}
+}
--- Group Project Game - Group 6/Assets/Scripts/Tile.cs
+++ Group Project Game - Group 6/Assets/Scripts/Tile.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour{
 
+	private GameManager gameManager;
 	private Vector3 target;
 	private bool moved;
+	public bool movable = true;
 
 	private void Start() {
+		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 		target = transform.position;
 	}
 
 	void OnMouseOver() {
-		if (Input.GetMouseButtonDown(0) && !moved) {
+		if (Input.GetMouseButtonDown(0) && !moved && gameManager.mana >= 100 && movable) {
+			gameManager.mana -= 100;
+			gameManager.manaText.text = gameManager.mana.ToString();
 			StartCoroutine(MoveTile());
 		}
 	}
