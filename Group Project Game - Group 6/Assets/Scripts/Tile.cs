87d93631d4fad5f84af0f82f90ab744be3774781
mrchooch <tyler-martignetti@hotmail.co.uk>
2019-02-12 15:33:40 +0000

    Tiles can be moved in various directions

--- Group Project Game - Group 6/Assets/Scripts/Tile.cs
+++ Group Project Game - Group 6/Assets/Scripts/Tile.cs
@@ -16,18 +16,44 @@ public class Tile : MonoBehaviour{
 
 	void OnMouseOver() {
 		if (Input.GetMouseButtonDown(0) && !moved && gameManager.mana >= 100 && movable) {
-			gameManager.mana -= 100;
-			gameManager.manaText.text = gameManager.mana.ToString();
 			StartCoroutine(MoveTile());
 		}
 	}
 
 	IEnumerator MoveTile() {
-		target = transform.position + new Vector3(0, 1, 0);
+		//Wait for the player to release mouse
+		yield return new WaitUntil(() => Input.GetMouseButtonUp(0));
+
+		//Get where mouse is
+		Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+		//Figure out what direction from the block it is
+		Vector2 heading = mousePos - (Vector2)transform.position;
+		Vector2 direction = heading / heading.magnitude;
+		direction = new Vector2(Mathf.Round(direction.x), Mathf.Round(direction.y));
+
+		//If diagonal, just choose one of the 2 directions randomly
+		if (direction.x != 0 && direction.y != 0) {
+			if (Random.Range(0, 1) == 0) {
+				direction = new Vector2(0, direction.y);
+			} else {
+				direction = new Vector2(direction.x, 0);
+			}
+		}
+
+		//Subtract mana
+		gameManager.mana -= 100;
+		gameManager.manaText.text = gameManager.mana.ToString();
+
+		//Move in that direction
+		target = (Vector2)transform.position + direction;
 		moved = true;
+
 		yield return new WaitForSeconds(5);
+
+		//Move back
 		moved = false;
-		target = transform.position - new Vector3(0, 1, 0);
+		target = (Vector2)transform.position - direction;
 	}
 
 	private void Update() {
