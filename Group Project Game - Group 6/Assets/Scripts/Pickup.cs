206a63ab0603b9945a3d32041c346bbded78e7ff
Tyler Martignetti <tyler-martignetti@hotmail.co.uk>
2019-02-13 11:52:46 +0000

    Added meeting minutes

---
+++ Group Project Game - Group 6/Assets/Scripts/Pickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+	private GameManager gameManager;
+	private void Start() {
+		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+	private void OnTriggerEnter2D(Collider2D collision) {
+		gameManager.mana += 500;
+		gameManager.manaText.text = gameManager.mana.ToString();
+		Destroy(gameObject);
+	}
+}
