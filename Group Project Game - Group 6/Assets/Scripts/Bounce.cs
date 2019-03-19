fb7f4d808d91915ac92c8127e2b93b63f48cd422
mrchooch <tyler-martignetti@hotmail.co.uk>
2019-03-19 22:10:38 +0000

    Added bounce block

---
+++ Group Project Game - Group 6/Assets/Scripts/Bounce.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour {
+	public int force = 500;
+
+	private void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.tag == "Player") {
+			collision.rigidbody.AddForce(Vector2.up * force);
+		}
+	}
+}
