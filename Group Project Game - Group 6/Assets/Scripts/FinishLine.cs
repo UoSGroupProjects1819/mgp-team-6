af69ff620907025611922a883d324a93e045bc86
mrchooch <tyler-martignetti@hotmail.co.uk>
2019-03-26 17:17:20 +0000

    Added Finish Line

---
+++ Group Project Game - Group 6/Assets/Scripts/FinishLine.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class FinishLine : MonoBehaviour
+{
+	public SceneAsset nextScene;
+
+	private void OnCollisionEnter2D(Collision2D collision) {
+		SceneManager.LoadScene(nextScene.name);
+	}
+}
---
+++ Post Logger/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleSheetsToUnity;
+
+public class GameManager : MonoBehaviour
+{
+
+	public string associatedSheet = "13ocI06h4tvvB7ZEtKXruvQdWIH40ZMfgIYHSfiw34r0";
+	public string associatedWorksheet = "March";
+
+	private void Start() {
+		SpreadsheetManager.Read(new GSTU_Search(associatedSheet, associatedWorksheet), UpdateAllAnimals);
+		SpreadsheetManager.ReadPublicSpreadsheet(new GSTU_Search(associatedSheet, associatedWorksheet),
+		UpdateAllAnimals);
+	}
+
+	void UpdateAllAnimals(GstuSpreadSheet spreadsheetRef) {
+		print(spreadsheetRef["A2"].value);
+	}
+}
