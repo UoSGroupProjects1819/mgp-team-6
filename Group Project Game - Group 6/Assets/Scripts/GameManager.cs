162e63d66f48699223e251de1e632991c760d8a2
mrchooch <tyler-martignetti@hotmail.co.uk>
2019-05-08 20:15:35 +0100

    Added Level 3 and "End Game" Screen

--- Group Project Game - Group 6/Assets/Scripts/GameManager.cs
+++ Group Project Game - Group 6/Assets/Scripts/GameManager.cs
@@ -15,11 +15,7 @@ public class GameManager : MonoBehaviour
 
 		if (Input.GetKeyDown("1")) {
 			abilityMode = 0;
-			print("Move");
-		} else if (Input.GetKeyDown("2")) {
-			abilityMode = 1;
-			print("Scale");
-		}
+		} 
 	}
 
 	public void GameOver() {
